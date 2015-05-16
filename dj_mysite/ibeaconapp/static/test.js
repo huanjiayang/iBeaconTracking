130bc0f4a0db7eaba8e648ccbe255b16ff5ab2df
sli2-14 <lsut573035185@gmail.com>
2015-05-16 20:30:06 +0100

    test.js

---
+++ dj_mysite/ibeaconapp/static/test.js
@@ -0,0 +1,10 @@
+var music_demo = [ 
+	              {
+	            	  type:'classical',
+	            	  content:["Beethoven","Mozart","Tchaikovsky"]
+	              },
+				  {
+	            	  type:'pop',
+	            	  content:["Beatles!!!","Corrs","Fleetwood Mac","Status Quo"]
+	              }
+	         ]
