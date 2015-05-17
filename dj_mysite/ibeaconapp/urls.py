2708eb04f8d44bc8d2aa4371b885fef2b057344b
huanjiayang <huanjiayang@hotmail.com>
2015-05-17 11:16:30 +0100

    bug fixing in ibeaconapp.urls, quotes no longer required

--- dj_mysite/ibeaconapp/urls.py
+++ dj_mysite/ibeaconapp/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 from ibeaconapp import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'views.index', name = 'index'),
+    url(r'^$', views.index, name = 'index'),
 )
