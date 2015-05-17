d4054cdb38a9ed0890d20306d95346c6920f4004
huanjiayang <huanjiayang@hotmail.com>
2015-05-17 11:40:55 +0100

    Move all javascript code into js file

--- dj_mysite/ibeaconapp/static/ibeaconapp/ibeaconapp.js
+++ dj_mysite/ibeaconapp/static/ibeaconapp/ibeaconapp.js
@@ -8,3 +8,36 @@ var music_demo = [
 	            	  content:["Beatles!!!","Corrs","Fleetwood Mac","Status Quo"]
 	              }
 	         ]
+
+
+function init(){
+	 var html = '';
+	 for(var i=0; i<music_demo.length; i++){
+		 html += '<option>';
+		 html += music_demo[i].type;
+		 html += '</option>';
+	 }
+	 
+	 document.getElementById("floorplan_sel").innerHTML = html;
+}
+
+var rec;
+
+function onFloorplanChange(index){
+	rec = index;
+	 var html = '';
+	 for(var i=0; i<music_demo[index].content.length; i++){
+		 html += '<option>';
+		 html += music_demo[index].content[i];
+		 html += '</option>';
+	 }
+	 document.getElementById("deployment_sel").innerHTML = html;
+}
+
+function onDeploymentChange(index){
+	
+}
+
+function onDatasetChange(index){
+	
+}
